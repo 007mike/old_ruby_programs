85f0b8e08b45893cd78f770fdb690b68054c5b05
System Administrator <root@Tarantula.local>
2015-12-31 15:38:01 -0500

    modified create file program to check if file exists before writing

--- create_file_and_write.rb
+++ create_file_and_write.rb
@@ -1,8 +1,28 @@
 # create_file_and_write.rb
 
-puts "Enter a .txt file name to create [eg. bill.txt]: "
 
+puts "Enter a .txt file name to create [eg. bill.txt]: "
 filename = gets.chomp.to_s
+
+
+# check if it exists and ask what to do
+response = ''
+
+if File.exists?(filename) 
+  puts "#{filename} exists already. Overwrite? [y/n]"
+  response = gets.chomp.to_s
+  while response != 'y'
+    if response == 'n'
+      puts "good bye"
+      exit
+    else 
+      puts "must be [y or n]"
+      response = gets.chomp.to_s
+    end
+  end
+
+end
+
 
 puts "Enter text followed by [enter]. Exit by typing [enter] only:"
 input = "init"
